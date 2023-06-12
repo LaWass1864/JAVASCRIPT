f0d8a48f786120f2583f99280361d90765920a9e
LaWass1864 <amwass4@hotmail.com>
2023-06-12 20:29:11 +0200

    DOM ok

--- Les bases/DOM/cours/index.js
+++ Les bases/DOM/cours/index.js
@@ -1,58 +1,19 @@
-// Selecteurs
-// querySelector = une méthode, c'est une fonction codée d'avance
-document.querySelector("h4").style.background = "red";
+// SELECTEURS
+// document.querySelector("h4").style.background = "yellow";
+// const baliseHTML = document.querySelector("h4");
 
-const baliseHtml = document.querySelector("h4");
-const h2 = document.querySelector("h2");
-const keypress = document.querySelector(".keypress");
-console.log(h2);
-h2.addEventListener("click", () => {
-  keypress.style.background = "orange";
-});
-
-// Evenement sur le click
-// Déclaration des variables
+// Click event
 const questionContainer = document.querySelector(".click-event");
-
-// Vérification
-// console.log(questionContainer);
-
-questionContainer.style.borderRadius = "150px";
-
-// Syntaxte de l'évenement comme un click, scroll...
-// questionContainer.addEventListener("click", () => {
-//     console.log("Coucou !");
-// });
-
-// changer le style de la division .click-event
-
-// questionContainer.addEventListener("click", () => {
-//     questionContainer.style.background = "red";
-//     questionContainer.style.border = "3px solid teal";
-// });
-
-// Plusieurs styles = vaut mieux injecter une classe avec toutes les proprieté qu'on veut lui mettre
-
-// questionContainer.addEventListener("click", () => {
-//     questionContainer.classList.add('question-clicked');
-// });
-
-// On veut enlever la .class au click avec le toggle
+const btn1 = document.querySelector("#btn-1");
+const btn2 = document.getElementById("btn-2");
+const response = document.querySelector("p");
 
 questionContainer.addEventListener("click", () => {
   questionContainer.classList.toggle("question-clicked");
 });
 
-const btn1 = document.querySelector("#btn-1");
-const btn2 = document.getElementById("btn-2");
-const response = document.querySelector("p");
-
-// Debuggage
-// console.log(btn1,btn2);
-
-// Revelation du <p>
 btn1.addEventListener("click", () => {
-  response.classList.toggle("show-response");
+  response.classList.add("show-response");
   response.style.background = "green";
 });
 
@@ -61,38 +22,28 @@ btn2.addEventListener("click", () => {
   response.style.background = "red";
 });
 
-// Ne pas zapper que l'id est superieur a la class et a la balise html
+/* <div> > #id > .class > baliseHTML */
 
-// ****************************************
-
-// Evenement sur la souris
-
-//On pointe cette souris
-
+//--------------------------------------------------
+// Mouse Events
 const mousemove = document.querySelector(".mousemove");
 
-console.log(mousemove);
-
-// On utilisera window au lieu de document, car c'est l'evenement au dessus de document
-// le e renferme toute une serie d'evenement, ca récupere toutes les données.
-// Ne pas oublier de mettre l'unité
 window.addEventListener("mousemove", (e) => {
   mousemove.style.left = e.pageX + "px";
-
   mousemove.style.top = e.pageY + "px";
 });
 
 window.addEventListener("mousedown", () => {
-  mousemove.style.transform = "scale(2) translate(-25%,-50%)";
-});
-window.addEventListener("mouseup", () => {
-  mousemove.style.transform = "scale(1) translate(-50%,-50%)";
-  mousemove.style.border = "2px solid yellow";
+  mousemove.style.transform = "scale(2) translate(-25%, -25%)";
 });
 
-// Mouse enter : quand la souris survol une zone
+window.addEventListener("mouseup", () => {
+  mousemove.style.transform = "scale(1) translate(-50%, -50%)";
+  mousemove.style.border = "2px solid teal";
+});
+
 questionContainer.addEventListener("mouseenter", () => {
-  questionContainer.style.background = "rgba(0, 0, 0, 0.6)";
+  questionContainer.style.background = "rgba(0,0,0,0.6)";
 });
 
 questionContainer.addEventListener("mouseout", () => {
@@ -100,22 +51,18 @@ questionContainer.addEventListener("mouseout", () => {
 });
 
 response.addEventListener("mouseover", () => {
-  response.style.transform = "rotate(9deg)";
+  response.style.transform = "rotate(2deg)";
 });
 
-//----------------------------------
-
+//---------------------------------------------------
 // KeyPress event
 
 const keypressContainer = document.querySelector(".keypress");
 const key = document.getElementById("key");
-const box = document.querySelector(".boxes");
 
-// Création de son en Javascript avec le parametre on peut mettre n'importe qu'elle lettre
 const ring = (key) => {
   const audio = new Audio();
   audio.src = key + ".mp3";
-  console.log(ring);
   audio.play();
 };
 
@@ -125,217 +72,189 @@ document.addEventListener("keypress", (e) => {
   if (e.key === "j") {
     keypressContainer.style.background = "pink";
   } else if (e.key === "h") {
-    btn2.style.background = "blue";
-  } else if (e.key === "x") {
-    box.style.background = "red";
+    keypressContainer.style.background = "teal";
+  } else {
+    keypressContainer.style.background = "red";
   }
-  ring(e.key);
+  if (e.key === "z") ring(e.key);
 });
 
-/*********************/
-
+//-------------------------------------------------
 // Scroll Event
 
 const nav = document.querySelector("nav");
 
 window.addEventListener("scroll", () => {
-  console.log(window.scrollY);
-
   if (window.scrollY > 120) {
     nav.style.top = 0;
   } else {
-    nav.style.top = "-60px";
+    nav.style.top = "-50px";
   }
 });
 
-// ------------------------------------------
-// Form Event
+//--------------------------------------------------
+// Form Events
 const inputName = document.querySelector('input[type="text"]');
 const select = document.querySelector("select");
 const form = document.querySelector("form");
-// Stocker ce qui est taper dans l'Input
 let pseudo = "";
 let language = "";
 
 inputName.addEventListener("input", (e) => {
   pseudo = e.target.value;
-  console.log(pseudo);
 });
 
 select.addEventListener("input", (e) => {
   language = e.target.value;
 });
 
-// Pour ne pas changer de page aprés avoir cliquer sur submit
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  // JS connait l'id cgv
 
   if (cgv.checked) {
     document.querySelector("form > div").innerHTML = `
-        <h3> Pseudo : ${pseudo} </h3>
-        <h4> Langage préféré : ${language}
-        `;
+      <h3>Pseudo : ${pseudo}</h3>
+      <h4>Langage préféré : ${language}</h4>
+    `;
   } else {
     alert("Veuillez accepter les CGV");
   }
 });
 
-// Evenement Load event. Il se déclenche une fois que toutes la page est chargée
-
+//------------------------------------------------
+// Load event
 window.addEventListener("load", () => {
-  console.log("Doc chargé !");
+  // console.log("Document Chargé !");
 });
 
-// -----------------------------------------
-
-//
+//------------------------------------------------
+// ForEach
+// const boxes = document.getElementsByClassName("box");
 const boxes = document.querySelectorAll(".box");
-
-console.log(boxes);
 
 boxes.forEach((box) => {
   box.addEventListener("click", (e) => {
-    e.target.style.transform = "scale(0.7)";
+    box.style.transform = "scale(0.7)";
   });
 });
 
-// Selectionner par ID, on peut déclarer l'ID par getElementbyId
-
-// addEventListener VS onClick
-// La méthode onClick s'utilise qu'une fois.
+//------------------------------------------------
+// addEventListener Vs onclick
 // document.body.onclick = function() {
-//     console.log('Coucou');
-// }
+//   console.log("Scroll !");
+// };
 
-// document.body.onscroll = () => {
-//     alert('ca marche!')
-// }
+// Bubbling => fin (de base l'eventlistener est paramétré en mode Bublbing)
+document.body.addEventListener("click", () => {
+  console.log("click 1 !");
+});
 
-// Bubling = > déclenchement a la fin
+// Usecapture
 document.body.addEventListener(
   "click",
   () => {
-    console.log("click num1");
-  },
-  
-);
-
-// UseCapture => déclenchement au début
-document.body.addEventListener(
-  "click",
-  () => {
-    console.log("click num2");
+    console.log("click 2 !");
   },
   true
 );
 
+// https://gomakethings.com/what-is-that-third-argument-on-the-vanilla-js-addeventlistener-method-and-when-do-you-need-it/
 
-//----------------------------------------------
-
+//-------------------------------------------------
 // Stop propagation
+// questionContainer.addEventListener("click", (e) => {
+//   alert("Test !");
+//   e.stopPropagation();
+// });
 
-questionContainer.addEventListener('click', (e) => {
-    alert('test');
-    e.stopPropagation();
-});
+// removeEventListener
 
-// RemoveEventListener : retirer un evenement
-
-// ----------------------------------
-
-// BOM : Browser object model
+//-------------------------------------------------
+// BOM
 
 // console.log(window.innerHeight);
 // console.log(window.scrollY);
-// window.open('http://google.com', "cours js", "height=600, with=300")
+// window.open("http://google.com", "cours js", "height=600, width=800");
+// window.close()
 
-// Evenement adossés à Window
-// window.alert('hello')
-// Confirm
+// Evénements adossés à Window
+// alert("hello");
 
-btn2.addEventListener('click', () => {
-    confirm("voulez-vous vraiment vous tromper ?"); 
-})
+// confirm
+btn2.addEventListener("click", () => {
+  confirm("Voulez vous vraiment vous tromper ?");
+});
 
-let answer;
-btn2.addEventListener('click', () => {
-  answer =  prompt("Entrez votre nom?"); 
+// prompt
+btn1.addEventListener("click", () => {
+  let answer = prompt("Entrez votre nom !");
 
-  questionContainer.innerHTML += "<h3> Bravo " + answer + "</h3>";
-})
+  questionContainer.innerHTML += "<h3>Bravo " + answer + "</h3>";
+});
 
-// setimeout : tp en millisecondes avant de déclencher, c'est un timer
-
+// Timer, compte à rebours
 setTimeout(() => {
-  questionContainer.style.background = "red";
-}, 4000);
+  questionContainer.style.borderRadius = "300px";
+}, 2000);
 
-// Execute un bout de code toutes les temps de secondes
 // let interval = setInterval(() => {
-//   document.body.innerHTML += 
-//   "<div class='box'><h2>Nouvelle boite !</h2></div>"
-//   success();
-// }, 3000);
+//   document.body.innerHTML += `
+//       <div class='box'>
+//         <h2>Nouvelle Boite !</h2>
+//       </div>
+//     `;
+// }, 4000);
 
-// Quand on clique sur le body, on arrete la methode
-document.body.addEventListener('click', (e) => {
-  clearInterval(interval);
- console.log(e.target)
-})
+// document.body.addEventListener("click", (e) => {
+//   e.target.remove();
+//   clearInterval(interval);
+// });
 
-// Location = pour rediriger vers des liens
+// Location
 // console.log(location.href);
 // console.log(location.host);
-// console.log(location.location.pathname);
+// console.log(location.pathname);
 // console.log(location.search);
-
-// location.replace("https://lequipe.fr");
+// location.replace("http://lequipe.fr");
 
 // window.onload = () => {
 //   location.href = "http://twitter.fr";
-// }
+// };
 
-// Objet Navigator
+// Navigator
+// console.log(navigator.userAgent);
 
-console.log(navigator.userAgent)
-
-// Il peut aussi nous localiser
 // https://developer.mozilla.org/fr/docs/Web/API/Geolocation/getCurrentPosition
 
-var options = {
-  enableHighAccuracy: true,
-  timeout: 5000,
-  maximumAge: 0
-};
+// var options = {
+//   enableHighAccuracy: true,
+//   timeout: 5000,
+//   maximumAge: 0,
+// };
 
-function success(pos) {
-  var crd = pos.coords;
+// function success(pos) {
+//   var crd = pos.coords;
 
-  document.body.innerHTML += ('Votre position actuelle est :');
-  document.body.innerHTML += (`Latitude : ${crd.latitude}`);
-  document.body.innerHTML += (`Longitude : ${crd.longitude}`);
-  document.body.innerHTML += (`La précision est de ${crd.accuracy} mètres.`);
-}
+//   console.log("Votre position actuelle est :");
+//   console.log(`Latitude : ${crd.latitude}`);
+//   console.log(`Longitude : ${crd.longitude}`);
+//   console.log(`La précision est de ${crd.accuracy} mètres.`);
+// }
 
-function error(err) {
-  console.warn(`ERREUR (${err.code}): ${err.message}`);
-}
+// function error(err) {
+//   console.warn(`ERREUR (${err.code}): ${err.message}`);
+// }
 
-navigator.geolocation.getCurrentPosition(success, error, options);
+// navigator.geolocation.getCurrentPosition(success, error, options);
 
-
-//HIstory
+// History
+// console.log(history);
 // window.history.back();
-//history.go(-3);
+// history.go(-2)
 
-
-let prenom = setInterval(() => {
-  document.body.innerHTML += `<h3>Wassila</h3>`
-}, 3000);
-
-
-
-document.body.addEventListener('click', () => {
-  clearInterval(prenom);
-})
+//------------------------------------------------
+// SetProperty
+window.addEventListener("mousemove", (e) => {
+  nav.style.setProperty("--x", e.layerX + "px");
+  nav.style.setProperty("--y", e.layerY + "px");
+});
