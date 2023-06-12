1b86de2658f7d7bd6edfeb6fb77fb8c7dde1c82c
LaWass1864 <amwass4@hotmail.com>
2023-06-13 00:41:16 +0200

    Bubble 21:33

---
+++ Bubble/index.js
@@ -0,0 +1,15 @@
+// Creation d'éléments
+const bubble = document.createElement("span");
+// On lui ajoute la classe bubble
+bubble.classList.add('bubble');
+document.body.appendChild(bubble);
+
+// randomiser les bulles
+const size = Math.random() * 200 + 100 + "px";
+bubble.style.height = size;
+bubble.style.width = size;
+
+bubble.style.top = Math.random() * 100 + 50 + "%";
+bubble.style.left = Math.random() * 100 + "%";
+
+bubble.style.setProperty('--left', Math.random() * 100 + "%")
